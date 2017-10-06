9dada3b85fb56ce6b1072b404e423de12597ba2d
王震 <1341046884@qq.com>
2017-10-06 15:07:32 +0800

    添加项目文件。

---
+++ first_cpp/image_main.cpp
@@ -0,0 +1,16 @@
+// first_cpp.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <opencv2/opencv.hpp>
+using namespace cv;
+int main()
+{
+	Mat img = imread("1.jpg");
+	imshow("载入的图片", img);
+	waitKey(6000);
+	return  0;
+}
